3a4bb03d2d90737c52f61432d81e0b95e9924e74
Aurélien GARNIER <au2001sfg@gmail.com>
2022-06-27 16:21:29 +0200

    Create hello world HTTP server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const http = require("http")
+const CommonLib = require("@hackathon-climat-05/common-lib")
+
+const PORT = 8080
+const HOST = "0.0.0.0"
+
+const server = http.createServer((req, res) => {
+    console.log(`${new Date().toISOString()} - ${req.socket.remoteAddress} - ${req.method} http://${req.headers.host}${req.url}`)
+
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    })
+    res.write(JSON.stringify({
+        hello: "world"
+    }))
+    res.end()
+})
+
+server.listen(PORT, HOST)
+console.log(`${new Date().toISOString()} - Running on http://${HOST}:${PORT}`)
