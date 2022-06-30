8726adac33445804dd954f3da16774a283eb3f72
Aurélien GARNIER <au2001sfg@gmail.com>
2022-06-30 03:10:38 +0200

    Convert to single quotes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import http from "http"
-// import CommonLib from "@hackathon-climat-05/common-lib"
+import http from 'http'
+// import CommonLib from '@hackathon-climat-05/common-lib'
 
-const PORT = parseInt(process.env.PORT || "8080", 10)
-const HOST = process.env.HOST || "0.0.0.0"
+const PORT = parseInt(process.env.PORT || '8080', 10)
+const HOST = process.env.HOST || '0.0.0.0'
 
 const server = http.createServer((req, res) => {
     console.log(`${new Date().toISOString()} - ${req.socket.remoteAddress} - ${req.method} http://${req.headers.host}${req.url}`)
 
     res.writeHead(200, {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
     })
     res.write(JSON.stringify({
-        hello: "world"
+        hello: 'world'
     }))
     res.end()
 })
